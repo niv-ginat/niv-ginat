dc8059c010985c846511d22376a48bc65d162e5e
niv-ginat <niv.ginat@gmail.com>
2024-05-22 14:21:01 +0200

    Please make these changes

--- js/logoShuffle.js
+++ js/logoShuffle.js
@@ -11,6 +11,26 @@ const logoCollectionImages = [
     "images/logo-shuffle/logo_rosel.jpg",
     "images/logo-shuffle/logo_the-lab.jpg",
     "images/logo-shuffle/logo_tradie.jpg",
+    "images/logo-shuffle/logo_bouklas.jpg",
+    "images/logo-shuffle/logo_cherry.jpg",
+    "images/logo-shuffle/logo_continuum",
+    "images/logo-shuffle/logo_dafne.jpg",
+    "images/logo-shuffle/logo_fiona.jpg",
+    "images/logo-shuffle/logo_ii.jpg",
+    "images/logo-shuffle/logo_intrunk.jpg",
+    "images/logo-shuffle/logo_lazoo.jpg",
+    "images/logo-shuffle/logo_m-logo.jpg",
+    "images/logo-shuffle/logo_master.jpg",
+    "images/logo-shuffle/logo_OC2019.jpg",
+    "images/logo-shuffle/logo_open.jpg",
+    "images/logo-shuffle/logo_solace.jpg",
+    "images/logo-shuffle/logo_squareone.jpg",
+    "images/logo-shuffle/logo_sv.jpg",
+    "images/logo-shuffle/logo_nite-records.jpg",
+    "images/logo-shuffle/logo_carla.jpg",
+    "images/logo-shuffle/logo_ucla.jpg",
+    "images/logo-shuffle/logo_tradie.jpg",
+    
     
 ];
 
